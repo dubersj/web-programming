654eaadf4d9e065bddb32ce10e1826648292001f
dubersj <sduber@kent.edu>
2024-02-21 17:26:34 -0500

    unfinished JSON table, Flask example

---
+++ topic-03-flask/hello.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_index():
+    me = "Steve D."
+    return f"<p>Hello, {me} from the World!</p>"
+
+@app.route("/goodbye")
+def get_goodbye():
+    return "<p>Goodbye, world!</p>"
+
+@app.route("/data")
+def get_data():
+    data = [
+        {"name":"suzy","type":"dog"},
+        {"name":"cindy","type":"cat"}
+    ]
+    return jsonify(data)
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory('.', path)
+
+@app.route("/api/status")
+def get_status():
+    data = [
+        {"name":"suzie","status":"fine"},
+        {"name":"cindy","status":"sad"},
+        {"name":"meg","status":"lame"}
+    ]
+    return jsonify(data)
