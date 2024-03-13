3dbe7cf701ff8f7ba1ca01e428dab164c1da86c0
dubersj <sduber@kent.edu>
2024-03-13 18:06:54 -0400

    bugs fixed

--- topic-07-basic-quotes/quotes.py
+++ topic-07-basic-quotes/quotes.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request #redirect
+from flask import Flask, render_template, request,redirect
 from mongita import MongitaClientDisk
 from bson import ObjectId
-
+import json
 app = Flask(__name__)
 
 # create Mongita client connection
@@ -21,11 +21,36 @@ def get_quotes():
         item["object"] = ObjectId(item["_id"])
     return render_template("quotes.html", data=data)
 
+
+
 @app.route("/delete", methods=["GET"])
 @app.route("/delete/<id>", methods=["GET"])
 def get_delete(id=None):
     if id:
         quotes_collection = quotes_db.quotes_collection
-        data = list(quotes_collection.find({_id:ObjectId(id)}))
-        quotes_collection.delete_one(_id=ObjectId(id))
-    return render_template("quotes.html", data=data)
+        data = list(quotes_collection.find({"_id":ObjectId(id)}))
+        quotes_collection.delete_one({"_id":ObjectId(id)})
+    return redirect("/quotes")
+
+@app.route("/edit", methods=["GET"])
+@app.route("/edit/<id>", methods=["GET"])
+def get_edit(id=None):
+    if id:
+        quotes_collection = quotes_db.quotes_collection
+        data = quotes_collection.find_one({"_id":ObjectId(id)})
+        return render_template("form.html", data=data)
+    else: return render_template("form.html")
+
+
+
+@app.route("/create", methods=["POST"])
+def get_create():
+    quotes_collection = quotes_db.quotes_collection
+    data = request.get_data()
+    
+    print(json.loads(data))
+    quotes_collection.insert_one(json.loads(data))
+    return redirect("/quotes")
+# adding /edit /edit/id
+# if form not filled correctly redirect back to edit
+# adding /create get/post
